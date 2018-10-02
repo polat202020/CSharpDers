729f76ace6b8ec815ae2df0e09248024ebd70016
OGUZHAN <oguz_2020@hotmail.com>
2018-10-02 21:10:01 +0300

    ilk proje

---
+++ Hesap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankamatikProje
+{
+   public  class Hesap
+    {
+        //Constracter
+        public Hesap()
+        {
+            Bakiye = 100;
+        }
+        //Encapsulation
+        private int _bakiye;
+        public  int Bakiye
+        {
+            get
+            {
+                return _bakiye;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Bakiye Sıfır Olamaz");
+                }
+                _bakiye = value;
+            }
+        }
+
+        public  void ParaCek()
+        {
+            Bakiye = Bakiye - 10; //parayı hesaptan düştük
+        }
+        public  void ParaYatir()
+        {
+            Bakiye = Bakiye + 10; //para
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BankamatikProje
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Hesap OguzunHesabi { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //oguzun hesabı ilk açılışta açıldı
+            OguzunHesabi = new Hesap();
+        }
+
+       
+
+
+        private void btn10Cek_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+
+                OguzunHesabi.ParaCek();
+                BakiyeGoster();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+         
+        }
+
+      
+
+        private void btn10Yatir_Click(object sender, RoutedEventArgs e)
+        {
+
+            OguzunHesabi.ParaYatir();
+            BakiyeGoster();
+        }
+
+        private void BakiyeGoster()
+        {
+           
+            tbBakiye.Text = (OguzunHesabi.Bakiye).ToString();
+        }
+
+    }
+}
